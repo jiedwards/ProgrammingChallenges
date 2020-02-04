06a4d10e6f121278081ff2ed7b2db0375196dd05
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-02-04 21:37:43 +0000

    updated tests to allow for more likes

--- CodeWars/WhoLikesItTests.py
+++ CodeWars/WhoLikesItTests.py
@@ -9,6 +9,8 @@ class mainFunctionTests(unittest.TestCase):
 			self.assertEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
 			self.assertEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
 			self.assertEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+			self.assertEqual(likes(['Maria Sharipova', 'Jacob', 'Mark', 'Max', 'John', 'Jacob T']), 'Maria Sharipova, Jacob and 4 others like this')
+
  
 if __name__=='__main__':
 	unittest.main()
