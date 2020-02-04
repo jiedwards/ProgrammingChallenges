cf18df056ee187afe0b6cc8745d9dd695097d775
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-02-04 21:28:18 +0000

    added test files for codewars challenge

---
+++ CodeWars/WhoLikesItTests.py
@@ -0,0 +1,15 @@
+import unittest
+
+class mainFunctionTests(unittest.TestCase):
+
+		def test_who_likes_it_validation(self):
+			from WhoLikesIt import likes
+			self.assertEqual(likes([]), 'no one likes this')
+			self.assertEqual(likes(['Peter']), 'Peter likes this')
+			self.assertEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+			self.assertEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+			self.assertEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+ 
+if __name__=='__main__':
+	unittest.main()
+
