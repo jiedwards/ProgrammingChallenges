a5e7f8eb075dcf48bf43a07bacc2e4a74c909688
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-02-05 21:31:52 +0000

    added functionality for phone number challenge

---
+++ CodeWars/CreatePhoneNumber.py
@@ -0,0 +1,28 @@
+"""
+https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example:
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+
+"""
+
+def create_phone_number(n):
+    
+    phone_number = '('
+    
+    for position, number in enumerate(n):
+        if number < 0: 
+            return False
+        
+        if position == 3:
+            phone_number += ') '
+        elif position == 6:
+            phone_number += '-'
+            
+        phone_number += str(number)
+
+    return phone_number
