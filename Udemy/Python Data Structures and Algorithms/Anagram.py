03fed123f74705f6eba16461bc6fdd2ab29e05d0
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-06-04 15:50:22 +0100

    add solution problem to file

--- Udemy/Python Data Structures and Algorithms/Anagram.py
+++ Udemy/Python Data Structures and Algorithms/Anagram.py
@@ -1,3 +1,17 @@
+import unittest
+'''
+Problem
+Given two strings, check to see if they are anagrams. An anagram is when the two strings can be written using the exact same letters (so you can just rearrange the letters to get a different phrase or word).
+
+For example:
+
+"public relations" is an anagram of "crap built on lies."
+
+"clint eastwood" is an anagram of "old west action"
+
+Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g".
+'''
+
 def anagram(s1,s2):
     my_dict = {}
     
